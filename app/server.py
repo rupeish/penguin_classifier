f98887558edf8d67e8ee7f1617c14a5984c3eeb9
rupeish <rupeshspatil26@gmail.com>
2020-05-02 19:10:16 +0530

    added port

--- app/server.py
+++ app/server.py
@@ -9,6 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
+Port = int(os.environ.get('PORT', 50000))
+
 export_file_url = 'https://drive.google.com/uc?export=download&id=1-Ae9BkriqQRCri4IWC60bPm-Y2nwwynP'
 export_file_name = 'export.pkl'
 
@@ -66,4 +68,4 @@ async def analyze(request):
 
 if __name__ == '__main__':
     if 'serve' in sys.argv:
-        uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
+        uvicorn.run(app=app, host='0.0.0.0', port=Port, log_level="info")
